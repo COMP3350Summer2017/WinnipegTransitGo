bf9ea19d8eeccd372ecba585b653e2510cf9ce01
DimaMukhin <dima9348@gmail.com>
2017-05-21 23:19:14 -0500

    Implemented API interface
    Implemented the API interface and added but stop related query support.

---
+++ app/src/main/java/comp3350/WinnipegTransitGo/apiService/TransitAPIProvider.java
@@ -0,0 +1,22 @@
+package comp3350.WinnipegTransitGo.apiService;
+
+import comp3350.WinnipegTransitGo.objects.BusStop;
+import retrofit2.Call;
+
+/**
+ * All available Winnipeg Transit API calls
+ *
+ * @author Dima Mukhin
+ * @version 1.0
+ * @since 2017-05-21
+ */
+public interface TransitAPIProvider {
+    // get a bus stops by its number
+    Call<TransitAPIResponse> getBusStop(int stopNumber);
+
+    // get bus stops near utm location
+    Call<TransitAPIResponse> getBusStops(String distance, int x, int y, boolean walking);
+
+    // get bus stops near geographic location
+    Call<TransitAPIResponse> getBusStops(String distance, String lat, String lon, boolean walking);
+}
