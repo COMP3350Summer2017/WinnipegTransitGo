a54b44c66e211a61091bd70ed9de84379d1dfaca
DimaMukhin <dima9348@gmail.com>
2017-05-21 23:18:24 -0500

    Added bus stop related objects

---
+++ app/src/main/java/comp3350/WinnipegTransitGo/objects/Location.java
@@ -0,0 +1,40 @@
+package comp3350.WinnipegTransitGo.objects;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Geographic and UTM location
+ *
+ * @author Dima Mukhin
+ * @version 1.0
+ * @since 2017-05-21
+ */
+public class Location {
+    private UTMLocation utm;
+
+    @SerializedName("geographic")
+    private GeographicLocation geo;
+
+    //region public getters
+    public String getUTMZone() { return utm.zone; }
+
+    public int getX() { return utm.x; }
+
+    public int getY() { return utm.y; }
+
+    public String getLatitude() { return geo.latitude; }
+
+    public String getLongitude() { return geo.longitude; }
+    //endregion
+
+    class UTMLocation {
+        String zone;
+        int x;
+        int y;
+    }
+
+    class GeographicLocation {
+        String latitude;
+        String longitude;
+    }
+}
---
+++ app/src/main/java/comp3350/WinnipegTransitGo/objects/Street.java
@@ -0,0 +1,22 @@
+package comp3350.WinnipegTransitGo.objects;
+
+/**
+ * Street
+ *
+ * @author Dima Mukhin
+ * @version 1.0
+ * @since 2017-05-21
+ */
+public class Street {
+    private int key;
+    private String name;
+    private String type;
+
+    //region public getters
+    public int getKey() { return key; }
+
+    public String getName() { return name; }
+
+    public String getType() { return type; }
+    //endregion
+}
