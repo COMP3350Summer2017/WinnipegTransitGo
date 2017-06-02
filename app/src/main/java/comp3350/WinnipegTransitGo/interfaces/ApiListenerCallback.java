cff5c2917dc5553cf24a0c1fb99aee9af2cf414b
Abdul-Rasheed Audu <auduarash@gmail.com>
2017-06-02 17:50:18 -0500

    Added bus stop listener method to apilistener

---
+++ app/src/main/java/comp3350/WinnipegTransitGo/interfaces/ApiListenerCallback.java
@@ -0,0 +1,16 @@
+package comp3350.WinnipegTransitGo.interfaces;
+
+import java.util.List;
+
+import comp3350.WinnipegTransitGo.objects.BusStop;
+import comp3350.WinnipegTransitGo.objects.TransitListItem;
+
+/**
+ * Created by habib on 2017-05-28.
+ */
+
+public interface ApiListenerCallback {
+    public void updateListView(List<TransitListItem> transitListItems);
+
+    public void updateStopsOnMap(List<BusStop> busStops);
+}
