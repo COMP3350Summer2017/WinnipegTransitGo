b16ba7483ae23e390fed503d2925d6e50b40e50c
Paul Artimowich <paul.artimowich@gmail.com>
2017-06-02 15:08:55 -0500

    Added custom listview rows and code to populate them with bus-stop data

--- app/src/main/java/comp3350/WinnipegTransitGo/presentation/DisplayAdapter.java
+++ app/src/main/java/comp3350/WinnipegTransitGo/presentation/DisplayAdapter.java
@@ -2,6 +2,7 @@ package comp3350.WinnipegTransitGo.presentation;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import comp3350.WinnipegTransitGo.R;
 import comp3350.WinnipegTransitGo.objects.Display;
 
 /**
@@ -40,13 +42,38 @@ public class DisplayAdapter extends ArrayAdapter<Display> {
         // TODO: 2017-05-31 add multiple rows and columns to show all the Display information
         Display currDisplay = listViewRows.get(position);
         if (currDisplay != null) {
-            TextView topText = (TextView) resultView.findViewById(comp3350.WinnipegTransitGo.R.id.toptext);
-            TextView bottomText = (TextView) resultView.findViewById(comp3350.WinnipegTransitGo.R.id.bottomtext);
-            if (topText != null) {
-                topText.setText("Bus stop name: " + currDisplay.getBusStopName());
+            TextView destination = (TextView) resultView.findViewById(R.id.destination);
+            TextView busNumber = (TextView) resultView.findViewById(R.id.bus_number);
+            TextView busStatus = (TextView) resultView.findViewById(R.id.bus_status);
+            TextView timeToNextArrival = (TextView) resultView.findViewById(R.id.time_to_next_arrival);
+            TextView busStopName = (TextView) resultView.findViewById(R.id.bus_stop_name);
+            TextView busStopNumber = (TextView) resultView.findViewById(R.id.bus_stop_number);
+            TextView laterArrivalTime = (TextView) resultView.findViewById(R.id.later_arrival_time);
+            if (destination != null) {
+                destination.setText(currDisplay.getBusStopDestination());
             }
-            if (bottomText != null) {
-                bottomText.setText("Bus stop number: " + currDisplay.getBusNumber());
+            if (busNumber != null) {
+                busNumber.setText(Integer.toString(currDisplay.getBusNumber()));
+            }
+            if (busStatus != null) {
+                busStatus.setText(currDisplay.getBusStatus());
+            }
+            if (timeToNextArrival != null) {
+                timeToNextArrival.setText(currDisplay.getBusTimeRemaining());
+            }
+            if (busStopName != null) {
+                busStopName.setText(currDisplay.getBusStopName());
+            }
+            if (busStopNumber != null) {
+                busStopNumber.setText(Integer.toString(currDisplay.getBusStopNumber()));
+            }
+            if (laterArrivalTime != null) {
+                StringBuilder builder = new StringBuilder();
+                List<String> futureArrivalTimes = currDisplay.getTimes();
+                for (String time : futureArrivalTimes) {
+                    builder.append(time + "\n");
+                }
+                laterArrivalTime.setText(builder.toString());
             }
         }
         return resultView;
