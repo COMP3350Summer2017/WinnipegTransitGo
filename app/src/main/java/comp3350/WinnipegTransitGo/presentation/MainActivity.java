747d30efa6ead5a5e4abec3e0b72ffa703f492b7
DimaMukhin <dima9348@gmail.com>
2017-06-09 18:55:14 -0500

    Merge branch 'master' of C:\Users\Dima\Documents\GitHub\TransitApplication with conflicts.

--- app/src/main/java/comp3350/WinnipegTransitGo/presentation/MainActivity.java
+++ app/src/main/java/comp3350/WinnipegTransitGo/presentation/MainActivity.java
@@ -1,6 +1,7 @@
 package comp3350.WinnipegTransitGo.presentation;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -9,68 +10,35 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
-import com.google.android.gms.maps.GoogleMap.OnCameraMoveStartedListener;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import comp3350.WinnipegTransitGo.businessLogic.TransitListGenerator;
 import comp3350.WinnipegTransitGo.R;
-import comp3350.WinnipegTransitGo.services.transitAPI.ApiListenerCallback;
-import comp3350.WinnipegTransitGo.services.database.Database;
-import comp3350.WinnipegTransitGo.services.database.DatabaseService;
-import comp3350.WinnipegTransitGo.services.location.OnLocationChanged;
-import comp3350.WinnipegTransitGo.businessLogic.TransitListPopulator;
-import comp3350.WinnipegTransitGo.objects.BusStop;
-import comp3350.WinnipegTransitGo.objects.TransitListItem;
-import comp3350.WinnipegTransitGo.services.location.LocationChangeListener;
+import comp3350.WinnipegTransitGo.apiService.TransitAPI;
+import comp3350.WinnipegTransitGo.apiService.TransitAPIProvider;
+import comp3350.WinnipegTransitGo.apiService.TransitAPIResponse;
+import comp3350.WinnipegTransitGo.constants.LocationConstants;
+import comp3350.WinnipegTransitGo.interfaces.LocationListenerCallback;
+import comp3350.WinnipegTransitGo.services.LocationListenerService;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
 
 
-public class MainActivity
-        extends AppCompatActivity
-        implements OnMapReadyCallback, OnLocationChanged,
-        OnCameraMoveStartedListener, OnCameraIdleListener, ApiListenerCallback {
+public class MainActivity extends Activity implements OnMapReadyCallback, LocationListenerCallback {
+
     private GoogleMap map;
-    private BusListViewFragment busListViewFragment;
-    TransitListPopulator listGenerator;
-
-    List<Marker> busStopMarkers = new ArrayList<>();
-    boolean userMovingCamera = false;
-    Database database;
-    SupportMapFragment mapFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(comp3350.WinnipegTransitGo.R.layout.activity_main);
 
-        //create and/or open database
-        database = DatabaseService.createDataAccess();
-        listGenerator = new TransitListGenerator(this, getString(R.string.winnipeg_transit_api_key));
-        busListViewFragment = (BusListViewFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.bus_list_view_fragment);
 
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        TransitAPIProvider apiProvider = TransitAPI.getAPI("234989572384");
+        //Call<TransitAPIResponse> response = apiProvider.getBusStops(200, 234234, 435345, true);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        DatabaseService.closeDataAccess();
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -80,14 +48,8 @@ public class MainActivity
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     1);
         } else {
-            setupMap();
             setUserLocation();
         }
-    }
-
-    private void setupMap() {
-        map.setOnCameraMoveStartedListener(this);
-        map.setOnCameraIdleListener(this);
     }
 
     @Override
@@ -97,8 +59,9 @@ public class MainActivity
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     setUserLocation();
                 } else {
-                    setDefaultLocation();
+                    //User did not give us location access
                 }
+                return;
             }
         }
     }
@@ -107,95 +70,21 @@ public class MainActivity
     /**
      * I expect the user to have granted the permission at this point.
      * The exception is guaranteed to never be thrown.
-     */
+     * */
     public void setUserLocation() throws SecurityException {
-        setDefaultLocation();
         map.setMyLocationEnabled(true);
-
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocationListener listener = LocationChangeListener.getLocationListener(this);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                database.getUpdateInterval(),
-                database.getMinimumDistanceBetweenUpdates(),
+        LocationListener listener = LocationListenerService.getLocationListener(this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                LocationConstants.minimumTimeBetweenUpdates,
+                LocationConstants.minimumDistanceBetweenUpdates,
                 listener);
     }
 
 
-    private void setDefaultLocation() {
-        LatLng defaultLatLng = new LatLng(database.getDefaultLatitude(), database.getDefaultLongitude());
-        Location newLocation = new Location("");
-        newLocation.setLatitude(defaultLatLng.latitude);
-        newLocation.setLongitude(defaultLatLng.longitude);
-        locationChanged(newLocation);
-    }
-
-
     @Override
-    public void locationChanged(Location location) {
-        LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 13));
-        busListViewFragment.clearListView();
-        listGenerator.populateTransitList(location.getLatitude() + "", location.getLongitude() + "");
-    }
-
-
-    public void updateStopsOnMap(List<BusStop> busStops) {
-        removeBusStopMarkers();
-
-        for (BusStop busStop : busStops) {
-            double lat = Double.parseDouble(
-                    busStop.getLocation().getLatitude()
-            );
-            double lon = Double.parseDouble(
-                    busStop.getLocation().getLongitude()
-            );
-            String snippet = busStop.getName();
-            LatLng stopLocation = new LatLng(lat, lon);
-            Marker busStopMarker = map.addMarker(new MarkerOptions()
-                    .position(stopLocation)
-                    .snippet(snippet)
-                    .title(snippet)
-            );
-            busStopMarkers.add(busStopMarker);
-        }
-    }
-
-    private void removeBusStopMarkers() {
-        for (Marker marker : busStopMarkers) {
-            marker.remove();
-        }
-        busStopMarkers.clear();
-    }
-
-
-    @Override
-    public void onCameraMoveStarted(int i) {
-        if (i == OnCameraMoveStartedListener.REASON_GESTURE || i == OnCameraMoveStartedListener.REASON_API_ANIMATION) {
-            userMovingCamera = true;
-        } else if (userMovingCamera) {
-            userMovingCamera = false;
-        }
-    }
-
-
-    private void updateLocationFromCamera() {
-        LatLng centrePosition = map.getCameraPosition().target;
-        Location newLocation = new Location("");
-        newLocation.setLatitude(centrePosition.latitude);
-        newLocation.setLongitude(centrePosition.longitude);
-        locationChanged(newLocation);
-    }
-
-    @Override
-    public void onCameraIdle() {
-        if (userMovingCamera) {
-            userMovingCamera = false;
-            updateLocationFromCamera();
-        }
-    }
-
-    @Override
-    public void updateListView(List<TransitListItem> displayObjects) {
-        busListViewFragment.updateListView(displayObjects);
+    public void makeUseOfNewLocation(Location location) {
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 13));
     }
 }
