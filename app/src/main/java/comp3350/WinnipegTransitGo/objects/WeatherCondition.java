e53ab5ef7ff8bbcbe868f76e16ab7d707e23931d
DimaMukhin <dima9348@gmail.com>
2017-06-23 16:59:58 -0500

    Added night time support
    Weather condition can now hold night time weather information

--- app/src/main/java/comp3350/WinnipegTransitGo/objects/WeatherCondition.java
+++ app/src/main/java/comp3350/WinnipegTransitGo/objects/WeatherCondition.java
@@ -1,17 +1,31 @@
 package comp3350.WinnipegTransitGo.objects;
 
 /**
- * Created by Unknown on 2017-06-21.
+ * WeatherCondition
+ *  Weather condition enum.
+ *  Holds all possible system specific weather conditions.
+ *  Any weather API provider mast map to this enum
+ *
+ *  Weather condition is mapped to a weather code.
+ *  weather code is used to get more information about the weather,
+ *  or for setting up weather presentation.
+ *
+ * @author Dima Mukhin
+ * @version 1.0
+ * @since 2017-06-20
  */
 
 public enum WeatherCondition {
-    init("clearsky"),
-    clear_sky("clearsky"),
-    few_clouds("fewclouds"),
+    init("clearsky_day"),
+    clear_sky_day("clearsky_day"),
+    clear_sky_night("clearsky_night"),
+    few_clouds_day("fewclouds_day"),
+    few_clouds_night("fewclouds_night"),
     scattered_clouds("scatteredclouds"),
     broken_clouds("brokenclouds"),
     shower_rain("showerrain"),
-    rain("rain"),
+    rain_day("rain_day"),
+    rain_night("rain_night"),
     thunderstorm("thunderstorm"),
     snow("snow"),
     mist("mist");
