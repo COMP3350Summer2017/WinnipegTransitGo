4afb9558b4001295a16fffcb4c93fa41b7d829c8
syedhabibcs <urrehmsh@myumanitoba.ca>
2017-06-25 23:24:06 -0500

    Class comment for messages

--- app/src/main/java/comp3350/WinnipegTransitGo/presentation/Messages.java
+++ app/src/main/java/comp3350/WinnipegTransitGo/presentation/Messages.java
@@ -9,6 +9,15 @@ import comp3350.WinnipegTransitGo.R;
 /**
  * Created by habib on 6/23/2017.
  */
+/**
+ * Messages class
+ * Used by presentation layer to display errors or warnings in
+ * a dialog box to the user.
+ *
+ * @author Syed Habib
+ * @version 1.0
+ * @since 2017-06-23
+ */
 
 public class Messages {
     public static void fatalError(final Activity owner, String message) {
